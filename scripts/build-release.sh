9e2506788c698369da850816ccf709cf9b8b2389
Edwin Biemond <biemond@gmail.com>
2014-07-29 16:17:18 +0200

    added release number

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -6,6 +6,11 @@ set -x
  
 RELEASE_VERSION=`ruby scripts/get-maven-release-version.rb $GO_PIPELINE_COUNTER`
 TAG="v$RELEASE_VERSION"
+
+mkdir artifacts
+
+echo "${RELEASE_VERSION}" > artifacts/version
+echo "${TAG}" > artifacts/tag
  
 mvn versions:set \
     --batch-mode \
